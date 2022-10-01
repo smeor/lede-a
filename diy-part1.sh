4e2863bc2113a9376c2191b8efbe600333415929
smeor <brillised@qq.com>
2022-10-01 09:30:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,5 @@
 #
 #国内常用OpenWrt软件包源码合集，每天自动更新
 #https://github.com/liuran001/openwrt-packages
+#echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
 echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
