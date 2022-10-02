233457d9121cfee7f15d953eedc6b66d475fbd22
smeor <brillised@qq.com>
2022-10-02 17:17:52 +0800

    Update diy-part1.sh

--- diy-sh/diy-part1.sh
+++ diy-sh/diy-part1.sh
@@ -22,4 +22,4 @@
 #echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
